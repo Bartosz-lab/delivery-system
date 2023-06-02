0bc98d354a802388a6eed96972cc326d8c122abd
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2023-06-03 01:55:09 +0200

    add docker

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -68,7 +68,7 @@ async fn main() -> Result<(), impl Error> {
                     .configure(delivery::app::api::report::config),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
