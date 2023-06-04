31fb344d20df67bfebf0793077e02a5fdc85ff2e
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2023-06-04 14:59:13 +0200

    init guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { authGuard } from './auth.guard';
+import { roleGuard } from './role.guard';
+
 const routes: Routes = [
   { path: '', component: MainPageComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: 'contact', component: ContactComponent, canActivate: [authGuard, roleGuard], data: { role: 'Admin' } },
 ];
 
 @NgModule({
